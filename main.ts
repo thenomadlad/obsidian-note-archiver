e00d0520fe98fafdded62fbfdc0abd7aba37614e
Aditya Dhara <adhara@wayfair.com>
2023-05-27 19:14:55 -0400

    Initial draft, making the basics work

--- main.ts
+++ main.ts
@@ -1,89 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	normalizePath,
+} from "obsidian";
+import { join, dirname } from "path";
 
-// Remember to rename these classes and interfaces!
+const ARCHIVE_FOLDER_GROUPINGS = ['NoGrouping', 'Year', 'Month'] as const;
+type ArchiveFolderGrouping = typeof ARCHIVE_FOLDER_GROUPINGS[number];
 
-interface MyPluginSettings {
-	mySetting: string;
+interface NoteArchiverSettings {
+	archiveFolderName: string;
+	grouping: ArchiveFolderGrouping;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: NoteArchiverSettings = {
+	archiveFolderName: "Archive",
+	grouping: 'NoGrouping',
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class NoteArchiverPlugin extends Plugin {
+	settings: NoteArchiverSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
+			id: "note-archiver-archive-current-note",
+			name: "Archive current note",
+			editorCheckCallback: (checking: boolean, editor: Editor, view: MarkdownView) => {
+				if (checking) {
+					return !view.file.path.startsWith(this.settings.archiveFolderName)
+				} else {
+					this.archivePage(view.file.path);
 					return true;
 				}
-			}
+			},
 		});
+
+		// on right-clicking a file
+		this.registerEvent(
+			this.app.workspace.on("file-menu", (menu, file) => {
+				if (!file.path.startsWith(this.settings.archiveFolderName)) {
+					menu.addItem((item) => {
+						item.setTitle("📤 Archive file")
+							.setIcon("document")
+							.onClick(async () => {
+								this.archivePage(file.path);
+							});
+					});
+				}
+			})
+		);
+
+		// on clicking the 3-dots on the top right of an editor
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+				menu.addItem((item) => {
+					let path = view.file?.path;
+
+					if (path && !path.startsWith(this.settings.archiveFolderName)) {
+						item.setTitle("📤 Archive file")
+							.setIcon("document")
+							.onClick(async () => {
+								this.archivePage(path ?? "");
+							});
+					}
+				});
+			})
+		);
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new NoteArchiverSettingTab(this.app, this));
 	}
 
-	onunload() {
+	onunload() {}
 
+	async archivePage(path: string) {
+		let targetFile = this.app.vault.getAbstractFileByPath(path) as TFile;
+
+		// get and create archive folder
+		let archiveFolder = this.settings.archiveFolderName;
+		if (this.settings.grouping === 'NoGrouping') {
+			archiveFolder = this.settings.archiveFolderName;
+		} else {
+			if (this.settings.grouping === 'Year') {
+				let year = new Date().getFullYear();
+				archiveFolder = normalizePath(join(this.settings.archiveFolderName, `${year}`));
+			} else if (this.settings.grouping === 'Month') {
+				let year = new Date().getFullYear();
+				let month = new Date().getMonth();
+				archiveFolder = normalizePath(join(this.settings.archiveFolderName, `${year}`, `${month}`));
+			}
+		}
+
+		// new path for archived file
+		let newPath = normalizePath(join(archiveFolder, path));
+
+		// make sure the folder for the file exists
+		let newFolder = dirname(newPath);
+		console.log("Attempting to check and create folder " + newFolder);
+		console.log(this.app.vault.getAbstractFileByPath(newFolder));
+		if (this.app.vault.getAbstractFileByPath(newFolder) === null) {
+			console.log("Attempting to create it");
+			await this.app.vault.createFolder(newFolder);
+		}
+
+		// move the file
+		await this.app.vault.copy(targetFile, newPath);
+		await this.app.vault.delete(targetFile);
+
+		new FileMoveNoticeModal(this.app, `${path} moved to ${newPath}`).open();
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -91,47 +132,91 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class FileMoveNoticeModal extends Modal {
+	text = "";
+	
+	constructor(app: App, text: string) {
 		super(app);
+		this.text = text;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText(this.text);
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+class NoteArchiverSettingTab extends PluginSettingTab {
+	plugin: NoteArchiverPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: NoteArchiverPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Settings for my awesome plugin." });
+
+		// folder path
+		let folderPathSetting = new Setting(containerEl)
+			.setName("Archive folder")
+			.setDesc("Where should I put your archived files?")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your secret")
+					.setValue(this.plugin.settings.archiveFolderName)
+					.onChange(async (value) => {
+						let folder = normalizePath(value);
+						this.plugin.settings.archiveFolderName = folder;
+						await this.plugin.saveSettings();
+
+						updateFolderPathHelpMessage(this.plugin.settings.archiveFolderName);
+					})
+			);
+
+		// helper message for folder path not existing
+		let folderPathHelpMessage = folderPathSetting.infoEl.createEl("p", {text: "", cls: ["setting-item-description", "setting-item-extra-info"]});
+		let updateFolderPathHelpMessage = (folder: string) => {
+			let abstractFile = this.app.vault.getAbstractFileByPath(normalizePath(folder));
+			console.log(abstractFile);
+			if (!abstractFile) {
+				folderPathHelpMessage.textContent = "Folder not in vault, it will be created when you archive a note here";
+			} else {
+				if (abstractFile instanceof TFile) {
+					folderPathHelpMessage.textContent = "File exists with this name, you can't archive anything until you change this";
+				} else {
+					folderPathHelpMessage.textContent = "Folder exists, all good";
+				}
+			}
+		}
+		updateFolderPathHelpMessage(this.plugin.settings.archiveFolderName);
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Group by")
+			.setDesc("Should I group your archived files?")
+			.addDropdown((dropdown) =>
+					dropdown
+						.addOption("NoGrouping", "Don't group my files")
+						.addOption("Year", "Group by year file is archived")
+						.addOption("Month", "Group by year and month file is archived")
+						.setValue(this.plugin.settings.grouping)
+						.onChange(async (value) => {
+							if (!ARCHIVE_FOLDER_GROUPINGS.find((validName) => value === validName)) {
+								throw new Error("Unable to parse ArchiveFolderGrouping from value " + value);
+							}
+
+							this.plugin.settings.grouping = value as ArchiveFolderGrouping;
+							await this.plugin.saveSettings();
+						})
+			);
 	}
 }
